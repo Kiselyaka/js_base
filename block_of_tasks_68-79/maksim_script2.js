b89564ed0bbf96b037b2cf4a4ac4761f1f92df53
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-12 15:15:12 +0300

    блок задач 68-79 И-2-23 Киселев Максим

--- block_of_tasks_68-79/maksim_script2.js
+++ block_of_tasks_68-79/maksim_script2.js
@@ -8,3 +8,18 @@ let arr_str68 = ['1', '2', '3'];
 alert(arr_str68);
 console.log(arr_str68);
 // глава 69
+let arr69_1 = ['1', '2', '3'];
+console.log(arr69_1[0]); 
+console.log(arr69_1[1]); 
+console.log(arr69_1[2]); 
+
+let arr69_2 = [1, 2, 3];
+console.log(arr69_2[0]); 
+console.log(arr69_2[1]); 
+console.log(arr69_2[2]); 
+
+let arr69_3 = [1, 2, 3];
+alert(arr69_3[0] + arr69_3[1] + arr69_3[2]);
+
+let arr69_4 = ['a', 'b', 'c', 'd'];
+alert(String(arr69_4[0] + '+' + arr69_4[1] + '+' + arr69_4[2] + '+' + arr69_4[3]));
