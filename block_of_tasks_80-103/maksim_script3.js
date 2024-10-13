23beb4daf10f2c215391e7a6d69c7fd65a943860
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-13 12:22:49 +0300

    блок задач 80-103 И-2-23 Киселев Максим

--- block_of_tasks_80-103/maksim_script3.js
+++ block_of_tasks_80-103/maksim_script3.js
@@ -3,3 +3,5 @@
 let obj80 = {1: 'monday', 2: 'tuesday', 3: 'wednesday', 4: 'thursday', 5: 'friday', 6: 'saturday', 7: 'sunday'};
 console.log(obj80);
 // глава 81
+let obj81 = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'};
+console.log(obj81);
