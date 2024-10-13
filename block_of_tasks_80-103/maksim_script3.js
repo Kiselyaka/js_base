d7e54a0eea0e0730b7967283fc71b35a1c61033f
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-13 12:42:41 +0300

    блок задач 80-103 И-2-23 Киселев Максим

--- block_of_tasks_80-103/maksim_script3.js
+++ block_of_tasks_80-103/maksim_script3.js
@@ -12,3 +12,50 @@ console.log(user82['name'], user82['surname'], user82['patronymic']);
 let date83 = {year: '2024', month: '10', day: '13'};
 console.log([date83.year, date83.month, date83.day].join('-'));
 // использовал метод join,чтобы был красивый вывд даты
+
+// глава 84
+/*
+Дан объект:
+
+let obj = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+Для каких ключей данного объекта кавычки обязательны, а для каких нет?
+
+Ответ: для 0,2,3
+*/
+
+/*
+Исправьте ошибки, допущенные в следующем коде:
+
+let obj = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+console.log(obj.1a);
+console.log(obj.b2);
+console.log(obj.c-c);
+console.log(obj.d 4);
+console.log(obj.e5);
+*/
+let obj_84 = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+console.log(obj_84['1a']);
+console.log(obj_84.b2);
+console.log(obj_84['с-с']);
+console.log(obj_84['d 4']);
+console.log(obj_84.e5);
