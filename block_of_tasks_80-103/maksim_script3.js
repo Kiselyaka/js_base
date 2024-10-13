2ae628b2f367de810e7b955667cf9af34e8b193b
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-13 12:35:03 +0300

    блок задач 80-103 И-2-23 Киселев Максим

--- block_of_tasks_80-103/maksim_script3.js
+++ block_of_tasks_80-103/maksim_script3.js
@@ -8,3 +8,7 @@ console.log(obj81);
 // глава 82
 let user82 = {name: 'Максим', surname: 'Киселев', patronymic: 'БимБим'};
 console.log(user82['name'], user82['surname'], user82['patronymic']);
+// глава 83
+let date83 = {year: '2024', month: '10', day: '13'};
+console.log([date83.year, date83.month, date83.day].join('-'));
+// использовал метод join,чтобы был красивый вывд даты
