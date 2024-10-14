6104c6a49bed0dd96b4ca77533feac644bcf6ec3
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-14 14:43:49 +0300

    блок задач 104-142 И-2-23 Киселев Максим

---
+++ block_of_tasks_104-142/maksim_script4.js
@@ -0,0 +1,23 @@
+"use strict"
+let test104 = 2;
+if (test104 > 10) {
+	console.log('+++'); // сработает это
+} else {
+	console.log('---');
+}
+if (test104 < 10) {
+	console.log('+++'); // сработает это
+} else {
+	console.log('---');
+}
+if (test104 >= 10) {
+	console.log('+++'); // сработает это
+} else {
+	console.log('---');
+}
+if (test104 <= 10) {
+	console.log('+++'); // сработает это
+} else {
+	console.log('---');
+}
+
