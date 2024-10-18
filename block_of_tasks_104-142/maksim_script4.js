aea19f0b345b7929e0212f16afb4f10a6fcbb3bf
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-19 00:03:43 +0300

    блок задач 104-142 И-2-23 Киселев Максим

--- block_of_tasks_104-142/maksim_script4.js
+++ block_of_tasks_104-142/maksim_script4.js
@@ -1228,3 +1228,230 @@ if (number140 % 3 === 0) {
 } else {
   console.log('Число не делится на 3');
 }
+
+// глава 141
+
+// №1⊗jsPmCdMst
+
+// Код должен проверить сумму чисел:
+
+// let num1 = 1;
+// let num2 = 2;
+
+// if (num1 + num2 === 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+// №2⊗jsPmCdMst
+
+// Код должен проверить сумму чисел:
+
+// let num1 = '1';
+// let num2 = '2';
+
+// if (num1 + num2 === 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+// №3⊗jsPmCdMst
+
+// Код должен проверить сумму чисел:
+
+// let num1 = '1';
+// let num2 = '2';
+
+// if (Number(num1 + num2) === 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+
+// №4⊗jsPmCdMst
+
+// Код должен проверить первую цифру числа:
+
+// let num = 123;
+
+// if (num[0] === 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+// №5⊗jsPmCdMst
+
+// Код должен проверить первую цифру числа:
+
+// let num = 123;
+
+// if (String(num[0]) === 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+
+
+// №6⊗jsPmCdMst
+
+// Код должен проверить первую цифру числа:
+
+// let num = 123;
+
+// if (String(num)[0] === 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+// №7⊗jsPmCdMst
+
+// Код должен проверить первую цифру числа:
+
+// let num = 123;
+// let first = String(num)[0];
+
+// if (first === 1) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+// №8⊗jsPmCdMst
+
+// Код должен проверить, что в числе ровно две цифры:
+
+// let num = 12;
+
+// if (num.length === 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+
+
+// №9⊗jsPmCdMst
+
+// Код должен проверить, что в числе ровно две цифры:
+
+// let num = 12;
+// let str = Number(str);
+
+// if (str.length === 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+// №10⊗jsPmCdMst
+
+// Код должен проверить, что в числе ровно две цифры:
+
+// let num = 12;
+
+// if (String(num.length) === 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+
+// №11⊗jsPmCdMst
+
+// Код должен проверить, что в числе ровно две цифры:
+
+// let num = 12;
+
+// if (String(num).length === String(2)) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+// №12⊗jsPmCdMst
+
+// Код должен проверить, что в числе ровно две цифры:
+
+// let num = 12;
+
+// if (String(num).lenght === 2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+
+
+
+
+
+// №13⊗jsPmCdMst
+
+// Код должен проверить равна ли сумма первых трех цифр числа сумме вторых трех чисел:
+
+// let num = '123033'; // берем в кавычки, чтобы обращаться к цифрам
+
+// let sum1 = num[0] + num[1] + num[2];
+// let sum2 = num[3] + num[4] + num[5];
+
+// if (sum1 == sum2) {
+// 	console.log('суммы равны');
+// } else {
+// 	console.log('суммы не равны');
+// }
