20604d5f1c9a83379a588ff86d258c9a4f78c72e
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-19 23:10:34 +0300

    блок задач 143-167 И-2-23 Киселев Максим

--- block_of_tasks_143-167/maksim_script5.js
+++ block_of_tasks_143-167/maksim_script5.js
@@ -4,3 +4,11 @@ let arr144 = ['a', 'b', 'c', 'd', 'e'];
 for (let elem of arr144){
     console.log(elem);
 }
+// глава 145
+let obj145 = {x: 1, y: 2, z: 3};
+for (let key in obj145) {
+	console.log(key); // выведет 'a', 'b', 'c'
+}
+for (let key in obj145) {
+	console.log(obj145[key]); // выведет 1, 2, 3
+}
