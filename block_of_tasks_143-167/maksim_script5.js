d42862efdd845878b632a5c0f26266a13600d85e
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-19 23:26:05 +0300

    блок задач 143-167 И-2-23 Киселев Максим

--- block_of_tasks_143-167/maksim_script5.js
+++ block_of_tasks_143-167/maksim_script5.js
@@ -12,3 +12,35 @@ for (let key in obj145) {
 for (let key in obj145) {
 	console.log(obj145[key]); // выведет 1, 2, 3
 }
+// глава 146
+let i_146 = 1;
+while(i_146 <= 100){
+    console.log(i_146)
+    i_146++;
+}
+
+let i_146_2 = 11;
+while(i_146_2 <= 33){
+    console.log(i_146_2);
+    i_146_2++;
+}
+
+let i_146_3 = 100;
+let counter_146 = 0;
+while (i_146_3 <= 1000){
+    console.log(i_146_3);
+    console.log(counter_146);
+    i_146_3 = (i_146_3 * 3);
+    counter_146++;
+}
+
+let num146 = 1; // начальное значение
+let iterations146 = 0; // счетчик итераций
+
+while (num146 <= 1000) {
+  num146 = num146 * 3;
+  iterations146++;
+}
+
+console.log('Результат:' + num146);
+console.log('Количество итераций:' + iterations146);
