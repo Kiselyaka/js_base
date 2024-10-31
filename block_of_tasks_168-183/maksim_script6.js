12d81a42a4e056ade45af813d069a1e63a73abe8
Kiselyaka <kiselyaka12s@gmail.com>
2024-10-31 18:15:19 +0300

    блок задач 168-183 И-2-23 Киселев Максим

--- block_of_tasks_168-183/maksim_script6.js
+++ block_of_tasks_168-183/maksim_script6.js
@@ -24,8 +24,8 @@ let arr169 = [
 	],
 ];
 console.log(arr169[0][0][0]+arr169[0][1][0]+arr169[0][0][1]+arr169[0][1][1]+arr169[1][0][0]+arr169[1][0][1]+arr169[1][1][0]+arr169[1][1][1]);
-// глава 170
 
+// глава 170
 let arr170 = [
     [
         1, 2, 3, [4, 5, [6, 7]]
@@ -35,3 +35,28 @@ let arr170 = [
     ]
 ];
 console.log('55');
+
+// глава 171
+
+let arr171_1 = [[1, 2, 3], [4, 5], [6]];
+let sum171_1 = 0;
+
+for (let subArr171_1 of arr171_1) {
+  for (let elem of subArr171_1) {
+    sum171_1 += elem;
+  }
+}
+
+console.log("Сумма элементов:", sum171_1);
+
+let arr171_2 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let sum171_2 = 0;
+
+for (let subArr1_171_2 of arr171_2) {
+  for (let subArr2_171_2 of subArr1_171_2) {
+    for (let elem of subArr2_171_2) {
+      sum171_2 += elem; 
+    }
+  }
+}
+console.log("Сумма элементов:", sum171_2);
