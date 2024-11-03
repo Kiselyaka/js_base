a88aa48a50165e59001e72a447edc3986d6f2d5b
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-03 12:08:48 +0300

    блок задач 168-183 И-2-23 Киселев Максим

--- block_of_tasks_168-183/maksim_script6.js
+++ block_of_tasks_168-183/maksim_script6.js
@@ -86,3 +86,43 @@ for (let i = 0; i < arr172_2.length; i++) {
 }
 
 console.log("Сумма элементов трехмерного массива:", sum172_2);
+
+// глава 173
+let arr173_1 = [];
+
+for (let i = 0; i < 3; i++) {
+  arr173_1[i] = []; 
+  
+  for (let j = 0; j < 5; j++) {
+    arr173_1[i].push(j + 1); 
+  }
+}
+
+console.log(arr173_1);
+
+
+let arr173_2 = [];
+
+for (let i = 0; i < 3; i++) {
+  arr173_2[i] = [];
+  
+  for (let j = 0; j < 4; j++) {
+    arr173_2[i].push('x'); 
+  }
+}
+
+console.log(arr173_2);
+
+
+let arr173_3 = [];
+
+for (let i = 0; i < 3; i++) {
+  arr173_3[i] = [];
+  for (let j = 0; j < 2; j++) {
+    arr173_3[i][j] = []; 
+    for (let k = 0; k < 5; k++) {
+      arr173_3[i][j].push(k + 1); 
+    }
+  }
+}
+console.log(arr173_3);
