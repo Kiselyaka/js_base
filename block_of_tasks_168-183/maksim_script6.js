6a18e6cb141fb317cc6f63b5e24c1fbf45fea234
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-04 22:29:27 +0300

    блок задач 168-183 И-2-23 Киселев Максим

--- block_of_tasks_168-183/maksim_script6.js
+++ block_of_tasks_168-183/maksim_script6.js
@@ -964,3 +964,85 @@ for (let obj of data179_3) {
     }
   }
 }
+
+// глава 180
+
+let employees180_1 = [
+  {
+    name: 'name1',
+    salary: 300,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+  },
+];
+
+for (let employee of employees180_1) {
+  console.log(employee.name + ' - ' + employee.salary);
+}
+
+let employees180_2 = [
+  {
+    name: 'name1',
+    salary: 300,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+  },
+];
+
+let totalSalary180_2 = 0;
+
+for (let employee of employees180_2) {
+  totalSalary180_2 += employee.salary;
+}
+
+console.log('Сумма зарплат всех работников: ' + totalSalary180_2);
+
+let employees180_3 = [
+  {
+    name: 'name1',
+    salary: 300,
+    age: 28,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+    age: 29,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+    age: 30,
+  },
+  {
+    name: 'name4',
+    salary: 600,
+    age: 31,
+  },
+  {
+    name: 'name5',
+    salary: 700,
+    age: 32,
+  },
+];
+
+let totalSalaryAbove30_1803 = 0;
+
+for (let employee of employees180_3) {
+  if (employee.age >= 30) {
+    totalSalaryAbove30_1803 += employee.salary;
+  }
+}
+
+console.log('Сумма зарплат работников старше или равных 30 лет: ' + totalSalaryAbove30_1803);
