c59fa198c004ada40bc2b3e8bb98f87e473829b9
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-09 11:35:06 +0300

    блок задач 184-199 И-2-23 Киселев Максим

---
+++ block_of_tasks_184-199/maksim_script7.js
@@ -0,0 +1,13 @@
+"use strict"
+// глава 183
+
+console.log(Math.pow(2, 10));
+
+console.log(Math.sqrt(245));
+
+let arr183 = [4, 2, 5, 19, 13, 0, 10];
+let sum183 = 0; 
+for (let index = 0; index < arr183.length; index++) {
+    let element = Math.pow(arr183[index], 3);
+    console.log(sum183);
+}
