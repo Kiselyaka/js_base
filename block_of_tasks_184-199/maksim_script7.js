f1f33c344fa6a01c55cf08f8323f251aaad021a0
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-10 17:17:23 +0300

    блок задач 184-199 И-2-23 Киселев Максим

--- block_of_tasks_184-199/maksim_script7.js
+++ block_of_tasks_184-199/maksim_script7.js
@@ -14,4 +14,28 @@ for (let i = 0; i < arr184.length; i++) {
 
 let result184 = Math.sqrt(sumOfCubes);
 console.log(result184);
-// 
+// глава 185
+
+let num1_185 = 379;
+let sqrt1_185 = Math.sqrt(num1_185);
+
+let rounded1 = Math.round(sqrt1_185);
+let roundedToTenths1 = sqrt1_185.toFixed(1);
+let roundedToHundredths1 = sqrt1_185.toFixed(2);
+
+console.log(rounded1); // Округлено до целых
+console.log(roundedToTenths1); // Округлено до десятых
+console.log(roundedToHundredths1); // Округлено до сотых
+
+let num2_185 = 587;
+let sqrt2_185 = Math.sqrt(num2_185);
+
+let roundedFloor2 = Math.floor(sqrt2_185);
+let roundedCeil2 = Math.ceil(sqrt2_185);
+
+let result2 = {
+    floor: roundedFloor2,
+    ceil: roundedCeil2
+};
+
+console.log(result2);
