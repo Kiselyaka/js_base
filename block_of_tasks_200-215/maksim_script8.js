2d5306ac8b44c9fd4aced98a8e8729a05ae6e1de
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-11 20:50:14 +0300

    блок задач 200-215 И-2-23 Киселев Максим

--- block_of_tasks_200-215/maksim_script8.js
+++ block_of_tasks_200-215/maksim_script8.js
@@ -416,3 +416,351 @@ function getProduct(arr) {
 // Примеры использования:
 console.log(getProduct([1, 2, 3, 4])); // 24
 console.log(getProduct([2, 3, 5])); // 30
+
+// глава 114
+
+// Поиск ошибок в коде с функциями JavaScript
+// В следующих задачах некоторый программист написал код и, возможно, допустил в нем ошибки. Вы должны проверить, делает ли код то, что описано. Если код работает некорректно, вы должны исправить ошибки.
+
+// №1⊗jsPmFBMst
+
+// Код должен найти сумму результатов работы двух функций:
+
+// function func1() {
+// 	console.log(3);
+// }
+// function func2() {
+// 	console.log(5);
+// }
+
+// console.log( func1() + func2() );
+
+function func1() {
+    return 3;
+  }
+  function func2() {
+    return 5;
+  }
+  
+console.log(func1() + func2());
+
+// №2⊗jsPmFBMst
+
+// Код должен найти сумму элементов массива:
+
+// function sum(arr) {
+// 	let res = 0;
+	
+// 	for (let elem of arr) {
+// 		res += elem;
+// 		return res;
+// 	}
+// }
+
+// console.log(sum([1, 2, 3, 4, 5]));
+
+function sum214(arr) {
+    let res = 0;
+    
+    for (let elem of arr) {
+      res += elem;
+    }
+    
+    return res; // Переносим return за пределы цикла
+  }
+  
+  console.log(sum214([1, 2, 3, 4, 5]));
+
+// №3⊗jsPmFBMst
+
+// Код должен найти сумму элементов массива, однако, ничего не выводит в консоль:
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function func(arr) {
+// 	let res = 0;
+	
+// 	for (let elem of arr) {
+// 		res += elem;
+// 	}
+	
+// 	console.log(res);
+// }
+
+let arr214_3 = [1, 2, 3, 4, 5];
+
+function func214_3(arr214_3) {
+  let res = 0;
+  
+  for (let elem of arr214_3) {
+    res += elem;
+  }
+  
+  console.log(res); // Теперь выводит результат
+}
+
+func214_3(arr214_3)
+
+// №4⊗jsPmFBMst
+
+// Код должен найти сумму результатов работы двух функций:
+
+// function func1() {
+// 	return 3;
+// }
+// function func2() {
+// 	return 5;
+// }
+
+// console.log( func1 + func2 );
+
+function func1_214_4() {
+    return 3;
+  }
+  function func2_214_4() {
+    return 5;
+  }
+  
+  console.log(func1_214_4() + func2_214_4());
+
+// №5⊗jsPmFBMst
+
+// Код должен найти сумму элементов массива:
+
+// let sum = sum([1, 2, 3, 4, 5]);
+// console.log(sum);
+
+// function sum(arr) {
+// 	let sum = 0;
+	
+// 	for (let elem of arr) {
+// 		sum += elem;
+// 	}
+	
+// 	return elem;
+// }
+
+function sum214_5(arr) {
+    let total_214 = 0; // Переименовываем переменную для избежания конфликта имен
+    
+    for (let elem of arr) {
+      total_214 += elem;
+    }
+    
+    return total_214; // Возвращаем сумму
+  }
+  
+  let totalSum = sum214_5([1, 2, 3, 4, 5]); // Переименовываем переменную для ясности
+  console.log(totalSum);
+
+// №6⊗jsPmFBMst
+
+// Код должен найти сумму элементов массива:
+
+// let res = sum([1, 2, 3, 4, 5]);
+// console.log(res);
+
+// function sum(arr) {
+// 	let sum = 0;
+	
+// 	for (let elem of arr) {
+// 		sum += elem;
+// 	}
+// }
+
+function sum214_6(arr) {
+    let total214_6 = 0;
+    
+    for (let elem of arr) {
+      total214_6 += elem;
+    }
+    
+    return total214_6; // Теперь возвращаем сумму
+  }
+  
+  let res214_6 = sum214_6([1, 2, 3, 4, 5]);
+  console.log(res214_6);
+// №7⊗jsPmFBMst
+
+// Функция добавляет ноль к числу от 1 до 9, а числа больше 9 возвращает без изменений:
+
+// function add(num) {
+// 	if (num <= 9) {
+// 		return '0' + num;
+// 	}
+// }
+
+function add(num) {
+    if (num <= 9) {
+      return '0' + num;
+    }
+    return num; 
+  }
+
+// №8⊗jsPmFBMst
+
+// Код должен найти сумму элементов массива:
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = sum(arr);
+// console.log(sum);
+
+// function sum(arr) {
+// 	let res = 0;
+	
+// 	for (let elem of arr) {
+// 		res += elem;
+// 	}
+	
+// 	return res;
+// }
+
+let arr214_8 = [1, 2, 3, 4, 5];
+let totalSum_214 = sum214_8(arr214_8);
+console.log(totalSum_214);
+
+function sum214_8(arr214_8) {
+  let res = 0;
+  for (let elem of arr214_8) {
+    res += elem;
+  }
+  return res; 
+}
+
+// №9⊗jsPmFBMst
+
+// Код должен найти сумму цифр числа:
+
+// let num = 12345;
+// let res = getDigitsSum(num);
+// console.log(res);
+
+// function getDigitsSum(num) {
+// 	let arr = num.split('');
+// 	let sum = 0;
+	
+// 	for (let elem of arr) {
+// 		sum += elem;
+// 	}
+	
+// 	return sum;
+// }
+
+let num_214 = 12345;
+let res_214 = getDigitsSum(num_214);
+console.log(res_214);
+
+function getDigitsSum(num_214) {
+  let arr = String(num_214).split(''); 
+  let sum = 0;
+  
+  for (let elem of arr) {
+    sum += Number(elem);
+  }
+  
+  return sum; 
+}
+// №10⊗jsPmFBMst
+
+// Код должен проверить число на то, что оно является простым:
+
+// console.log(isPrime(13)); // должен вывести true
+
+// function isPrime(num) {
+// 	for (let i = 2; i < num; i++) {
+// 		if (num % i !== 0) {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+
+function isPrime(num) 
+{
+    if (num <= 1) return false; 
+    for (let i = 2; i < Math.sqrt(num) + 1; i++) { 
+      if (num % i === 0) {
+        return false; 
+      }
+    }
+    return true; 
+}
+console.log(isPrime(13));
+
+// глава 215
+
+function sumArrayElements215(arr215_1) {
+    let sum = 0;
+    for (let elem of arr215_1) {
+      sum += elem;
+    }
+    return sum;
+  }
+console.log(sumArrayElements215([1, 2, 3, 4]));
+
+
+function getDivisors215(num) {
+    let divisors = [];
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) {
+        divisors.push(i);
+      }
+    }
+    return divisors;
+  }
+console.log(getDivisors215(100));
+
+
+function stringToArray215(str) {
+    return str.split('');
+  }
+
+function reverseString215(str) {
+    return str.split('').reverse().join('');
+  }
+
+function capitalizeFirstLetter215(str) {
+    if (str.length === 0) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+function capitalizeFirstLetterOfEachWord215(str) {
+    return str.split(' ').map(capitalizeFirstLetter).join(' ');
+  }
+
+function fillArrayWithIntegers215(n) 
+{
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+      arr.push(i);
+    }
+    return arr;
+} 
+
+function sumOfDigits215(num) {
+    return String(num)
+      .split('')
+      .reduce((sum, digit) => sum + Number(digit), 0);
+  }
+
+function isLeapYear215(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  }
+
+function secondsToDays215(seconds) {
+    return Math.floor(seconds / (60 * 60 * 24));
+  }
+
+function getRandomElement215(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+function isPrime215(num) 
+{
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+}
