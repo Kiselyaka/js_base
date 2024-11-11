732209aa75ab1c99f36f384806c70aa5bdeb07f3
Kiselyaka <kiselyaka12s@gmail.com>
2024-11-11 22:31:44 +0300

    блок задач 216-222 И-2-23 Киселев Максим

---
+++ block_of_tasks_216-222/maksim_script9.js
@@ -0,0 +1,236 @@
+"use strict"
+// глава 216
+
+// №1⊗jsPmFVGb
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+		
+// function func() {
+// 	console.log(num);
+// }
+
+// func();
+
+// Ответ: 1
+
+// №2⊗jsPmFVGb
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+		
+// function func() {
+// 	console.log(num);
+// }
+
+// num = 2;
+// func();
+
+// Ответ: 2
+
+// №3⊗jsPmFVGb
+
+// Определите, не запуская код, что выведется в консоль:
+
+// function func() {
+// 	console.log(num);
+// }
+
+// let num;
+
+// num = 1;
+// func();
+
+// num = 2;
+// func();
+
+// Ответ:1
+//       2
+
+// глава 217
+
+// №1⊗jsPmFVLc
+
+// Определите, не запуская код, что выведется в консоль:
+
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+
+// console.log(num);
+
+// Ответ: ошибка
+
+// №2⊗jsPmFVLc
+
+// Определите, не запуская код, что выведется в консоль:
+
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+
+// console.log(func());
+
+// Ответ: 5
+
+// глава 218
+
+// №1⊗jsPmFVNC
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+// func();
+
+// console.log(num);
+
+// Ответ:2
+//       2
+
+// №2⊗jsPmFVNC
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	let num = 2;
+// }
+// func();
+
+// console.log(num);
+
+// Ответ: 2
+//        1
+
+// глава 219
+
+// №1⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+// func();
+
+// console.log(num);
+
+// Ответ: 2
+
+// №2⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+
+// console.log(num);
+
+// Ответ: 1
+
+// №3⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	let num = 2;
+// }
+// func();
+
+// console.log(num);
+
+// Ответ:2
+//       1
+
+// №4⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	let num = 2;
+// }
+
+// console.log(num);
+// func();
+
+// Ответ:1
+//       2
+
+// №5⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+
+// console.log(num);
+// func();
+
+// Ответ: 1
+//        2
+
+// №6⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// let num = 1;
+
+// function func() {
+// 	num++;
+// }
+
+// func();
+// func();
+// func();
+// console.log(num);
+
+// Ответ: 4
+
+// №7⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// function func() {
+// 	num = 2;
+// }
+
+// let num = 1;
+// console.log(num);
+// func();
+
+// Ответ: 1
+
+// №8⊗jsPmFVGVCh
+
+// Определите, не запуская код, что выведется в консоль:
+
+// function func() {
+// 	num = 2;
+// }
+
+// let num = 1;
+// func();
+// console.log(num);
+
+// Ответ: 2
